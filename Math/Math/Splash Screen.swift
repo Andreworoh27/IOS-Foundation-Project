3fd43bfa61020d5552f4ac88929ba5024413b758
Andrew Oroh <andreworoh.ao@gmail.com>
2023-07-07 13:42:43 +0700

    Create splash screen with loading animation

---
+++ Math/Math/Splash Screen.swift
@@ -0,0 +1,66 @@
+//
+//  Splash Screen.swift
+//  Math
+//
+//  Created by Andrew Oroh on 07/07/23.
+//
+
+import SwiftUI
+
+struct Splash_Screen: View {
+    @State private var isLoading = true
+    @State private var loadingValue = 0.00
+    @State private var redirectToAuthPage = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                ZStack {
+                    Image("SplashScreenBackground")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    VStack {
+                        Image("SpashScreenMascot")
+                            .scaleEffect(0.5)
+                        
+                        if isLoading {
+                            ProgressView(value: loadingValue)
+                                .progressViewStyle(LinearProgressViewStyle())
+                                .animation(.easeInOut(duration: 5.0))
+                            Text("Loading...")
+                                .padding(.top, 10)
+                        }
+                    }
+                }
+                .onAppear {
+                    startLoading()
+                }
+                .navigationBarHidden(true)
+                .background(
+                    NavigationLink(destination: AuthenticationPage(), isActive: $redirectToAuthPage) {
+                        EmptyView()
+                    }
+                )
+            }
+        }
+    }
+    func startLoading() {
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            loadingValue += 0.1
+            
+            if loadingValue >= 1.0 {
+                timer.invalidate()
+                redirectToAuthPage = true
+            }
+        }
+        
+    }
+}
+
+struct Splash_Screen_Previews: PreviewProvider {
+    static var previews: some View {
+        Splash_Screen()
+    }
+}
