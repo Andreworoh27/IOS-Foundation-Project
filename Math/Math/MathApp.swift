1b5746efbd0041f8c46e65a1d32887708df7b5f3
Andrew Oroh <andreworoh.ao@gmail.com>
2023-07-07 12:43:49 +0700

    Init File

---
+++ Math/Math/MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathApp.swift
+//  Math
+//
+//  Created by Andrew Oroh on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
