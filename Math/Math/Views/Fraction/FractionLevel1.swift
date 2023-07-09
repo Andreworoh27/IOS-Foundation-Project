d8563ea9c324855f4176d4572ee0a2715d3173cc
Andrew Oroh <andreworoh.ao@gmail.com>
2023-07-09 15:07:47 +0700

    Refactor folder in view, create dynamic navigation for fraction map

---
+++ Math/Math/Views/Fraction/FractionLevel1.swift
@@ -0,0 +1,20 @@
+//
+//  FractionLevel1.swift
+//  Math
+//
+//  Created by Andrew Oroh on 09/07/23.
+//
+
+import SwiftUI
+
+struct FractionLevel1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FractionLevel1_Previews: PreviewProvider {
+    static var previews: some View {
+        FractionLevel1()
+    }
+}
